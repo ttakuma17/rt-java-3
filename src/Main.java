ea26359d51fae3026f0726d026c1b8ce5926ab4d
ttakuma17 <ttanimiya@gmail.com>
2022-12-25 09:59:18 +0900

    例外処理の練習

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,24 @@ public class Main {
         pokemonParty.remove(2);
         System.out.println(pokemonParty);
 
+        pokemonParty.add("Bulbasaur");
+        pokemonParty.add("Charmander");
+        pokemonParty.add("Squirtle");
+
+        // ポケモンを6体以上連れて行こうとした時の例外処理
+        try{
+            if(pokemonParty.size() >= 6){
+                throw new NumberOfPokemonLimitException();
+            }else {
+                pokemonParty.add("Squirtle");
+                System.out.println("Squirtleを手持ちに追加しました。");
+            }
+        }catch(NumberOfPokemonLimitException e){
+            int target = pokemonParty.size() - 1;
+            pokemonParty.remove(target);
+            System.out.println("ポケモンを6体以上連れて歩くことはできません。");
+        }
+
         Map<String, String> pokemonInfo = new HashMap<>();
         pokemonInfo.put("Sprigatito","It can use its plant powers to soak up whatever water you throw at it");
         pokemonInfo.put("Quaxly","The burning-hot sparks it scatters will fry any and all plants in a flash");
@@ -24,5 +42,13 @@ public class Main {
         pokemonInfo.remove("Sprigatito");
         System.out.println(pokemonInfo);
 
+        // Hashmapに指定したキー：val が存在しない時、例外としてメッセージを表示させる。 null表示をさせない
+        try {
+            String val = "Picachu";
+            if(pokemonInfo.get(val) == null) throw new NullPointerException();
+            else System.out.println(pokemonInfo.get(val));
+        }catch (Exception e){
+            System.out.println("指定したkeyは存在しません。keyの名前が正しいか確認してください。");
+        }
     }
 }
